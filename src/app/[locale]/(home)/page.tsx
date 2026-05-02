cc52de422a1786d54de31671978268a5884997ee
Oumayma Bahous <bahousoumayma@gmail.com>
2026-05-02 15:55:46 +0100

    fml

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -21,9 +21,9 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
       <Vision />
       <Differentiation />
-      <About />
       <ServicesSection />
       <WhoWeWorkWith />
       <FAQ />
